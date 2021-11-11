eebdcb8d96ebfd4450c5a1373055fb593134e14a
Jay-JYKim91 <jay.jykim91@gmail.com>
2021-11-12 05:09:29 +0900

    edited

--- wicked/main.js
+++ wicked/main.js
@@ -11,31 +11,31 @@ let currentYear = now.getFullYear();
 year.innerText = currentYear;
 
 // 포트폴리오 메뉴 작은 버전
-window.addEventListener('scroll', function() {
-  let offSet = window.pageYOffset;
-  // console.log(offSet);
+// window.addEventListener('scroll', function() {
+//   let offSet = window.pageYOffset;
+//   // console.log(offSet);
 
-  let customOffset = document.getElementById('portfolio__custom').getBoundingClientRect().top + offSet;
-  let readymadeOffset = document.getElementById('portfolio__readymade').getBoundingClientRect().top + offSet;
-  let processOffset = document.getElementById('portfolio__education-process').getBoundingClientRect().top + offSet;
-  let finishOffset = document.querySelector('.about').getBoundingClientRect().top + offSet - 700;
+//   let customOffset = document.getElementById('portfolio__custom').getBoundingClientRect().top + offSet;
+//   let readymadeOffset = document.getElementById('portfolio__readymade').getBoundingClientRect().top + offSet;
+//   let processOffset = document.getElementById('portfolio__education-process').getBoundingClientRect().top + offSet;
+//   let finishOffset = document.querySelector('.about').getBoundingClientRect().top + offSet - 700;
 
-  if (document.documentElement.scrollTop > finishOffset) {
-    document.getElementById("menu_education-process").style.top = "-80px";
-    document.getElementById("menu_readymade").style.top = "-80px";
-    document.getElementById("menu_custom").style.top = "-80px";
-  } else if (document.documentElement.scrollTop > processOffset) {
-    document.getElementById("menu_education-process").style.top = "0";
-  } else if (document.documentElement.scrollTop > readymadeOffset) {
-    document.getElementById("menu_education-process").style.top = "-80px";
-    document.getElementById("menu_readymade").style.top = "0";
-  } else if (document.documentElement.scrollTop > customOffset) {
-    document.getElementById("menu_readymade").style.top = "-80px";
-    document.getElementById("menu_custom").style.top = "0";
-  } else {
-    document.getElementById("menu_custom").style.top = "-80px";
-  }
-});
+//   if (document.documentElement.scrollTop > finishOffset) {
+//     document.getElementById("menu_education-process").style.top = "-80px";
+//     document.getElementById("menu_readymade").style.top = "-80px";
+//     document.getElementById("menu_custom").style.top = "-80px";
+//   } else if (document.documentElement.scrollTop > processOffset) {
+//     document.getElementById("menu_education-process").style.top = "0";
+//   } else if (document.documentElement.scrollTop > readymadeOffset) {
+//     document.getElementById("menu_education-process").style.top = "-80px";
+//     document.getElementById("menu_readymade").style.top = "0";
+//   } else if (document.documentElement.scrollTop > customOffset) {
+//     document.getElementById("menu_readymade").style.top = "-80px";
+//     document.getElementById("menu_custom").style.top = "0";
+//   } else {
+//     document.getElementById("menu_custom").style.top = "-80px";
+//   }
+// });
 
 // console.log(customOffset);
 // console.log(customOffset + window.pageYOffset);
